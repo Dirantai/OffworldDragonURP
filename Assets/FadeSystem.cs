3a493504223bc838620c1317bba0ad377411b070
Dirantai <pixl.nate@gmail.com>
2023-01-02 15:41:19 +0000

    Added Fade In
    A fade in was added to be used as "Loading Screens"

---
+++ Assets/FadeSystem.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeSystem : MonoBehaviour
+{
+    private Image fade;
+    private float timer;
+    public float fadeDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fade = GetComponent<Image>();
+        timer = fadeDuration + 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0){
+            timer -= Time.deltaTime;
+        }else{
+            Destroy(gameObject);
+        }
+        Color fadeColour = new Color(0, 0, 0, timer / fadeDuration);
+        fade.color = fadeColour;
+    }
+}
